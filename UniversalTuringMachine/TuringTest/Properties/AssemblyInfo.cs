b1bfd7ce87e09aabe40d8dc514224883b50f78dd
Zanidd <ninijay93@gmail.com>
2018-04-26 11:47:53 -0700

    Added Busy Beaver test

---
+++ UniversalTuringMachine/TuringTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TuringTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TuringTest")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("1ce6e4ba-beff-4884-9b59-270466ffb262")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UniversalTuringMachine/TuringTest/TestMachine.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using UniversalTuringMachine;
+
+namespace TuringTest
+{
+    [TestClass]
+    public class TestMachine
+    {
+        [TestMethod]
+        public void TestBusyBeaver()
+        {
+            List<State> states = new List<State>();
+            List<State> accept = new List<State>();
+            State start;
+            List<char> alpha = new List<char>();
+            List<Calc> sigi = new List<Calc>();
+            Tape tp = new Tape();
+            UniversalTuringMachine.UniversalTuringMachine machine;
+
+
+            // add states
+            states.Add(new State("q0", 0));
+            states.Add(new State("q1", 1));
+            states.Add(new State("qf", 2));
+            accept.Add(states[2]);
+            start = states[0];
+            alpha.Add('0');
+            alpha.Add('1');
+
+            sigi.Add(new Calc(states[0], states[1], 1, '0', '1'));
+            sigi.Add(new Calc(states[0], states[1], -1, '1', '1'));
+            sigi.Add(new Calc(states[1], states[0], -1, '0', '1'));
+            sigi.Add(new Calc(states[1], states[2], 1, '1', '1'));
+
+            tp.Left = new List<char>();
+            tp.Right = new List<char>();
+            for (int x=0; x < 10; x++)
+            {
+                tp.Left.Add('0');
+                tp.Right.Add('0');
+            }
+
+            machine = new UniversalTuringMachine.UniversalTuringMachine(states, new List<char>(), alpha, sigi, start, accept, tp);
+            machine.Perform();
+        }
+    }
+}
